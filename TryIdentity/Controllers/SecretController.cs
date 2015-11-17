ddabde3d80ee673c8107a56c53319446a7b0d1cd
knolegsb <seon2you@hotmail.com>
2015-11-16 16:01:49 -0800

    start and added secret controller

---
+++ TryIdentity/Controllers/SecretController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TryIdentity.Controllers
+{
+    public class SecretController : Controller
+    {
+        [Authorize]
+        public ContentResult Secret()
+        {
+            return Content("This is a secret...");
+        }
+
+        public ContentResult Overt()
+        {
+            return Content("This is not a secret...");
+        }
+        // GET: Secret
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
